0b7b99ad1940e6ee5c9b421154d9ff4402c65083
Kat.Davidson <70635747+KateDavidson@users.noreply.github.com>
2020-10-24 22:19:05 +0200

    Update hr_controller.py

--- controller/hr_controller.py
+++ controller/hr_controller.py
@@ -3,10 +3,11 @@ from view import terminal as view
 
 
 def list_employees():
-    for index, list_employees in enumerate(list_employees, start = 1):
-        view.print(index,list_employees)
-    #view.print_error_message("Not implemented yet.")
-    # jak nie chcemy ponumerowanych usowamy start = 1
+    #for index, list_employees in enumerate(list_employees, start = 1):
+        #view.print(index,list_employees)
+    view.print_error_message("Not implemented yet.")
+   #not ready yet :)
+    
 
 def add_employee():
     view.print_error_message("Not implemented yet.")
