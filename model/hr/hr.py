2eef7964ceafeb3803307c14cc1029184b71aa5d
Kat.Davidson <70635747+KateDavidson@users.noreply.github.com>
2020-10-25 10:45:40 +0100

    Update hr.py

--- model/hr/hr.py
+++ model/hr/hr.py
@@ -12,5 +12,5 @@ from model import data_manager, util
 
 DATAFILE = "model/hr/hr.csv"
 HEADERS = ["Id", "Name", "Date of birth", "Department", "Clearance"]
-def list_employees():
-    list_of_employees = {'Alcie','Cecil','Bob'}
+def get_employees():
+     return [["Jz6J5&jw<r","Alice","2000-01-01","Sales","3"]['45+ohJm&dB','Bob','1989-10-13','Production','6']['ßl0W_tbm5Z','Cecil','1993-04-04','Sales','5']]
